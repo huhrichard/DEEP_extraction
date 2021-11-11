52fff01cd031aa6eefce661d515e87aa7b6f459b
Richard <richardpokard@gmail.com>
2021-11-11 13:17:29 -0500

    no message

--- submit_deep_main_jobs.py
+++ submit_deep_main_jobs.py
@@ -3,12 +3,12 @@ import os
 
 suffix = sys.argv[-1]
 outcome_binary_dict = {
-    # 'resampled_daily_controller_past6months': True,
-    # 'resampled_emergency_dept': True,
-    # 'resampled_hospitalize_overnight': True,
-    'daily_controller_past6months': True,
-    'emergency_dept': True,
-    'hospitalize_overnight': True,
+    's5_daily_controller_past6months': True,
+    's5_emergency_dept': True,
+    's5_hospitalize_overnight': True,
+    # 'daily_controller_past6months': True,
+    # 'emergency_dept': True,
+    # 'hospitalize_overnight': True,
 }
 
 for outcome in outcome_binary_dict:
@@ -16,7 +16,7 @@ for outcome in outcome_binary_dict:
               "#BSUB -n 4\n#BSUB -W 2:00\n#BSUB -o analyzer_%J.stdout\n#BSUB -eo analyzer_%J.stderr\n" \
               "#BSUB -R rusage[mem=10000]\nmodule purge\n"
     outcome_replaced = outcome.replace('(', '\(').replace(')', '\)')
-    python_cmd = "python deep_main.py --filename {}.csv --outcome {} --result_dir {}".format(outcome, outcome, 'paper_result_')
+    python_cmd = "python deep_main.py --filename {}.csv --outcome {} --result_dir {}".format(outcome, outcome, 's5_result_')
 
     lsf_name = "{}.lsf".format(outcome)
     lsf_replaced_name = "{}.lsf".format(outcome_replaced)
