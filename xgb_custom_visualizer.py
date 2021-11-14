59ff4ca18e4228ade5fbd755424a1310031047f2
Richard <richardpokard@gmail.com>
2021-11-14 14:58:34 -0500

    no message

--- xgb_custom_visualizer.py
+++ xgb_custom_visualizer.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.tree import export_graphviz
-from sklearn.externals.six import StringIO
+from six import StringIO
 import pydotplus
 
 def check_dir(target_dir):
